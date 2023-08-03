e9d536bdd990c0eb2b068170cf736d02d976a1c3
HenriqueNapimo1 <henriquenapimo1@gmail.com>
2023-08-03 19:15:09 -0300

    botão de informações adicionado

--- scripts/main.js
+++ scripts/main.js
@@ -74,32 +74,6 @@ document.addEventListener('DOMContentLoaded', async () => {
     createFutureFilmElements();
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Get all anchor tags with the class "openModal"
-    const openModalLinks = document.querySelectorAll(".openModal");
-
-    // Get the modal and the modal body element
-    const modal = document.getElementById("myModal");
-    const modalBody = modal.querySelector(".modal-body");
-
-    // Attach click event listener to each anchor tag
-    openModalLinks.forEach((link) => {
-        link.addEventListener("click", function (event) {
-            event.preventDefault();
-            // Get the content ID from the data-content-id attribute
-            const contentID = this.dataset.contentId;
-            // Use the content ID to fetch the content or generate it
-             // You need to implement this function to get the content based on the ID
-            // Update the modal body with the content
-            modalBody.innerHTML = getContentById(contentID);
-
-            // Show the modal
-            const bootstrapModal = new bootstrap.Modal(modal);
-            bootstrapModal.show();
-        });
-    });
-});
-
 // Example function to fetch content based on the ID (You can modify this according to your needs)
 function getContentById(contentID) {
     // Example: assuming you have an object with content based on IDs
@@ -175,17 +149,24 @@ function generateFilmeElement(filme, dia) {
     filmeElement.innerHTML = `
             <div class="filme-header">
                 <h3>${filme.titulo}</h3>
-                <a href="javascript:void(0)" class="openModal">
+                <a data-bs-toggle="collapse" href="#${filme.uuid}" role="button" aria-expanded="false" aria-controls="${filme.uuid}">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="26" viewBox="0 0 25 25" fill="none">
                         <path d="M11.25 6.27621H13.75V8.77621H11.25V6.27621ZM11.25 11.2762H13.75V18.7762H11.25V11.2762ZM12.5 0.0262146C5.6 0.0262146 0 5.62621 0 12.5262C0 19.4262 5.6 25.0262 12.5 25.0262C19.4 25.0262 25 19.4262 25 12.5262C25 5.62621 19.4 0.0262146 12.5 0.0262146ZM12.5 22.5262C6.9875 22.5262 2.5 18.0387 2.5 12.5262C2.5 7.01371 6.9875 2.52621 12.5 2.52621C18.0125 2.52621 22.5 7.01371 22.5 12.5262C22.5 18.0387 18.0125 22.5262 12.5 22.5262Z" fill="#F8EBFF"/>
                     </svg>
                 </a>
             </div>
+            <div class="collapse" id="${filme.uuid}">
+                <div class="card card-body text-center" style="background-color: #1A0E46">
+                    <span class="small">${filme.tempo} &bull; CLASS. ${filme.class} &bull; EM CARTAZ AT&Eacute; ${filme.fim}</span>
+                    <p>${filme.sinopse}</p>
+                    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${filme.trailer}" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <span class="small">MAIS INFORMA&Ccedil;&Otilde;ES:</span>
+                    <span class="small"><bold>DIRE&Ccedil;&Atilde;O:</bold> ${filme.direcao} | <bold>ROTEIRO:</bold> ${filme.roteiro} | <bold>ATORES:</bold> ${filme.atores} | <bold>G&Ecirc;NERO:</bold> ${filme.genero} | <bold>IMDB:</bold> ${filme.imdb}</span>
+                </div>
+            </div>
             <div class="filme-body">
                 <div class="cartaz">
-                    <a href="javascript:void(0)" class="openModal">
-                        <img src="images/${filme.uuid}_poster.jpg" alt="Poster de $titulo" class="img-cartaz">
-                    </a>
+                    <img src="images/${filme.uuid}_poster.jpg" alt="Poster de $titulo" class="img-cartaz">
                 </div>
                 <div class="dados">
                     <div class="sessoes">
